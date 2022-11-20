06ebe9f0fc36296adbac5411a6a57f20c2e3cb26
NiCo7889 <113980882+NiCo7889@users.noreply.github.com>
2022-11-20 22:33:15 +0100

    cambio

--- EJERCICIO3.py
+++ EJERCICIO3.py
@@ -6,15 +6,16 @@ paises["alemania"]
 
 from ast import main 
 
-def error_elemento_fuera_lista(lista, elemento_lista): #Creo una función para la resolución del ejercicio
-    try: #Pruebo a ver si funciona
-        elemento = lista[elemento_lista]
-        return elemento 
-    except IndexError:
-        print ("El elemento {} no se encuentra en la lista, ya que la lista solo contiene {} elementos".format(elemento_lista, len(lista)))     
+def error_clave_fuera_diccionario(diccionario, clave):#Creo una función para la resolución del ejercicio
+    try:#Pruebo a ver si funciona
+        clave = diccionario[clave]
+        return clave 
+    except KeyError:
+        print ("La clave {} no se encuentra en la lista y por tanto no se puede devolver su valor correspondiente".format(clave))     
     return 
 
-error_elemento_fuera_lista([4, 7, 30, 23, 5], 10)#Ejecuto la función
+error_clave_fuera_diccionario({ "españa":"español", "eeuu":"inglés", "italia":"italiano" } , "alemania")#Ejecuto la función
+
 
 if __name__ == "__main__":
     main()
