0815a6158bc1dd46bca9617d4766e6ac84e760c3
NiCo7889 <113980882+NiCo7889@users.noreply.github.com>
2022-11-20 21:28:43 +0100

    cambio

--- EJERCICIO2.py
+++ EJERCICIO2.py
@@ -4,3 +4,20 @@ lista = [4, 7, 30, 23, 5]
 lista[10]
 """
 
+from ast import main
+
+print("Ejercicio 2:\n")
+
+def error_elemento_fuera_lista(lista, elemento_lista):
+    try:
+        elemento = lista[elemento_lista]
+        return elemento
+    except IndexError:
+        print("No se puede acceder al elemento {} de lista porque solo tiene {} elementos".format(elemento_lista, len(lista)))
+
+error_elemento_fuera_lista([4, 7, 30, 23, 5], 10)
+
+print("\n")
+
+if __name__ == "__main__":
+    main()
