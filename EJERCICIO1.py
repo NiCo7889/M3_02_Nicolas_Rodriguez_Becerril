3fa3b3c07a5b8f82dde9ce18557f4f340d383e94
NiCo7889 <113980882+NiCo7889@users.noreply.github.com>
2022-11-20 21:28:52 +0100

    cambio

--- EJERCICIO1.py
+++ EJERCICIO1.py
@@ -3,15 +3,17 @@ Código a evaluar:
 numero = 7/0
 """
 
-def division_por_cero():    
-    try:                    
-       numero = 7/0
-    except ZeroDivisionError:   
-        return "No se puede dividir entre 0"    
-    return numero
-    
-def main():
-    print(division_por_cero())
+from ast import main 
+
+def error_division_0(numerador,denominador):
+    try: 
+        numero = numerador/denominador
+        return numero 
+    except ZeroDivisionError:
+        print("No se puede dividir por 0")
+    return
+
+error_division_0(7,0)
 
 if __name__ == "__main__":
     main()
